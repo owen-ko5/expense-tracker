1ccb75c442740da6ae4347b8624c21ad77e8c9a5
Owen <owenkaris9512@gmail.com>
2025-04-14 22:39:24 +0300

    form added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Expense Tracker</h1>
+      <p>Start taking control of your finaces and files. Record, categorize and analyze your spending </p>
+      <form>
+        <label htmlFor="Enter expense name"></label>
+        <input type="text" placeholder= "Expense Name" />
+
+        <label htmlFor="Enter expense description"></label>
+        <input type="text" placeholder= "Expense Name" />
+
+        <label htmlFor="Enter expense category"></label>
+        <input type="text" placeholder= "Expense Name" />
+
+        <label htmlFor="enter amount"></label>
+        <input type="text" placeholder= "Expense Name" />
+
+        <label htmlFor="mm/dd/yy"></label>
+        <input type="text" placeholder= "Expense Name" />
+
+      </form>
+      
     </div>
   );
 }
